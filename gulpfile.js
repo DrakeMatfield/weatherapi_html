6609ef2a5fca0450553ba1cb8c185d9cabeaef73
Drake Matfield <drakematfield@yahoo.com>
2018-05-18 18:34:24 +0000

    added gitignore file to keep things clean
     On branch class_version
     Changes to be committed:
    	new file:   .gitignore
    	new file:   dist/StyleSheet.css
    	new file:   dist/css/fontawesome-all.min.css
    	new file:   dist/favicon.ico
    	new file:   dist/images/cloud.jpg
    	new file:   dist/index.html
    	new file:   dist/webfonts/fa-brands-400.eot
    	new file:   dist/webfonts/fa-brands-400.svg
    	new file:   dist/webfonts/fa-brands-400.ttf
    	new file:   dist/webfonts/fa-brands-400.woff
    	new file:   dist/webfonts/fa-brands-400.woff2
    	new file:   dist/webfonts/fa-regular-400.eot
    	new file:   dist/webfonts/fa-regular-400.svg
    	new file:   dist/webfonts/fa-regular-400.ttf
    	new file:   dist/webfonts/fa-regular-400.woff
    	new file:   dist/webfonts/fa-regular-400.woff2
    	new file:   dist/webfonts/fa-solid-900.eot
    	new file:   dist/webfonts/fa-solid-900.svg
    	new file:   dist/webfonts/fa-solid-900.ttf
    	new file:   dist/webfonts/fa-solid-900.woff
    	new file:   dist/webfonts/fa-solid-900.woff2
    	modified:   gulpfile.js
    	modified:   index.html
    	new file:   indexpro.html
    	modified:   package.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,58 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var maps = require('gulp-sourcemaps');
+var del = require('del');
 
-gulp.task("minifyScripts", function(){
-gulp.src('app.js').pipe(uglify()).pipe(rename("app.min.js")).pipe(gulp.dest(""))
+gulp.task("concatScripts", function() {
+    return gulp.src([
+        'ISO_DateFormat.js',
+        'Weather.js',
+        'analytical.js',
+        'ParserWeatherYahooApi.js',
+        'JScript.js',
+        'weatherApi.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest(''));
 });
 
-gulp.task("concatScripts", function(){
-gulp.src(['JScript.js','JScriptNotifier.js']).pipe(concat("app.js")).pipe(gulp.dest(""))
+gulp.task("minifyScripts", ["concatScripts"], function() {
+  return gulp.src("app.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest(''));
 });
 
-gulp.task("hello", function(){
-console.log("Hello!");
+gulp.task('compileSass', function() {
+  return gulp.src("scss/application.scss")
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('css'));
 });
 
-gulp.task("default",["hello"], function(){
-console.log("This is the default task!");
+gulp.task('watchSass', function() {
+  gulp.watch('scss/**/*.scss', ['compileSass']);
+})
+
+gulp.task('clean', function() {
+  del(['dist', 'app*.js*']);
+});
+
+gulp.task("build", ['minifyScripts'], function() {
+  return gulp.src(["StyleSheet.css", "app.min.js", 'index.html',
+                   "css/fontawesome-all.min.css",
+                   "favicon.ico",
+                  "images/cloud.jpg",
+                  "webfonts/*"
+                  ], { base: './'})
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task("default", ["clean"], function() {
+  gulp.start("build");  
 });
